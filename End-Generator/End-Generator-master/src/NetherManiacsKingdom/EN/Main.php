1567af6046a4a47c8a41de5e58e8050f8654f052
DavidJBrockway <twitter@pocketserver.co.uk>
2016-02-27 21:23:53 +0000

    Added files via upload
    working now

---
+++ End-Generator/End-Generator-master/src/NetherManiacsKingdom/EN/Main.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NetherManiacsKingdom\EN;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\generator\Generator;
+
+class Main extends PluginBase {
+	
+	public function onEnable(){
+		Generator::addGenerator(EndGenerator::class, EndGenerator::NAME);
+	}
+}
